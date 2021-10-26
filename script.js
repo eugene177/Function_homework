74c9b1792ebca20325f26b9831752eef7a7663b3
eugene177 <vbxer2019@gmail.com>
2021-10-26 10:14:15 +0300

    Last commit

---
+++ script.js
@@ -0,0 +1,30 @@
+// // Задача №1
+// function getSquare(number) {
+//   return number * number
+// }
+// console.log(getSquare(2))
+
+// // Задача №2
+// function randomCount(firstNum, secondNum, thirdNum) {
+//   if (isNaN(firstNum) || isNaN(secondNum) || isNaN(thirdNum)) {
+//     return 'Only numbers!'
+//   } else {
+//     return (firstNum - secondNum) / thirdNum
+//   }
+// }
+// console.log(randomCount(1, 2, 1))
+
+// // Задача №3
+// let arr = [
+//   'Понедельник',
+//   'Вторник',
+//   'Среда',
+//   'Четверг',
+//   'Пятница',
+//   'Суббота',
+//   'Воскресенье',
+// ]
+// const getDay = num => {
+//   return arr[num - 1]
+// }
+// console.log(getDay(5))
